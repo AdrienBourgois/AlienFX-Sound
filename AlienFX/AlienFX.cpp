a6ceb9ec9b26323e0dc478b2a7fa78a9bb6021d9
Monk <adrien.bourgois@sfr.fr>
2016-06-14 03:57:53 +0200

    Change to SetLightColor (don't work)

--- AlienFX/AlienFX.cpp
+++ AlienFX/AlienFX.cpp
@@ -21,18 +21,23 @@ int main()
 		afx.reset();
 		std::cout << "Reset" << std::endl;
 
-		//unsigned int numDevs = 0;
-		//std::cout << "Error : " << afx.getNumDevices(&numDevs) << std::endl;
-		//std::cout << "Devices : " << numDevs << std::endl;
+		unsigned int numDevs = 0;
+		std::cout << "Error : " << afx.getNumDevices(&numDevs) << std::endl;
+		std::cout << "Devices : " << numDevs << std::endl;
 
 		LFX_COLOR color;
-		color.red = 255;
+		color.red = 0;
 		color.green = 255;
-		color.blue = 255;
-		color.brightness = 0;
+		color.blue = 0;
+		color.brightness = 255;
 
-		if (afx.light(LFX_ALL, LFX_RED | LFX_FULL_BRIGHTNESS) == LFX_SUCCESS)
+		//if (afx.light(LFX_ALL, LFX_RED | LFX_FULL_BRIGHTNESS) == LFX_SUCCESS)
+			//std::cout << "Light" << std::endl;
+		LFX_RESULT result = afx.setLightColor(0, LFX_ALL, &color);
+		if (result == LFX_SUCCESS)
 			std::cout << "Light" << std::endl;
+		else
+			std::cout << "Light Error : " << result << std::endl;
 		if (afx.update() == LFX_SUCCESS)
 			std::cout << "Update" << std::endl;
 
